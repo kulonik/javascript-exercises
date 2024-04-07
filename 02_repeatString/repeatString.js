37cb34b07d046ad6f30cdc31eef9013e569e6f81
kulonik <annakhmv@gmail.com>
2024-04-07 22:18:48 +0500

    solve the task in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = 0;
+    let newStr = "";
+    while(i < num) {
+        newStr += string;
+        i++;
+    }
+    return newStr;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
