f13c4eb0d6c800f4e3a3aabc8d8c4d33f26cf11a
kulonik <annakhmv@gmail.com>
2024-04-10 12:48:20 +0500

    Add solution in leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(1997));
 
 // Do not edit below this line
 module.exports = leapYears;
