264e2fa7464c3a739f06cc332ec1aba0e35523cb
kulonik <annakhmv@gmail.com>
2024-04-10 16:06:16 +0500

    Add solution in tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let degreeF = (degree - 32) * 5 / 9;
+  return Math.round(degreeF * 10) / 10;
 };
+console.log(convertToCelsius(100));
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(degree) {
+  let degreeC = (degree * 9 / 5 + 32);
+  return Math.round(degreeC * 10) / 10;
 };
+console.log(convertToFahrenheit(73.2));
 
 // Do not edit below this line
 module.exports = {
