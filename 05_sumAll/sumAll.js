c1892c7dc76306a173aab668818031294356b826
kulonik <annakhmv@gmail.com>
2024-04-10 10:56:54 +0500

    Add solution in sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    for(i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
